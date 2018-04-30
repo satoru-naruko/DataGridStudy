2a64207b2820fb394d1212ce791dc1b1887cf56e
鳴子智 <naruko505@gmail.com>
2018-04-30 23:40:07 +0900

    DataGridサンプルコードをコミット ・AutoGenerateColumns=False にて手動でカラムデータを作成
    ViewModelにてコマンドを実装
    ・DataGridにデータ追加するコマンドを実装
    ・データが10個以上になれば実行しないように実装（CanExecute）

---
+++ DataGridStudy/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+
+namespace DataGridStudy
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        static public ObservableCollection<Tweet> Tweets { get; set; } = new ObservableCollection<Tweet>();
+        public MainWindowViewModel()
+        {
+            Tweets.Add(new Tweet("naruko1", "test1", "https://etstesa/"));
+            Tweets.Add(new Tweet("naruko2", "test2", "https://etstesa/"));
+            Tweets.Add(new Tweet("naruko3","test3", "https://etstesa/"));
+        }
+
+        // Interfaices
+        public DelegateCommand AddCommand { get; } = new DelegateCommand(
+            () => { Tweets.Add(new Tweet()); },
+            () => Tweets.Count <= 10
+        );
+    }
+
+}
